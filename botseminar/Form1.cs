f7d767f42bb5c052f02a6d87618a320f0024a52e
Георгий Кузнецов <georgekitkat23@gmail.com>
2021-07-06 11:13:05 +0300

    вывод друзей

--- botseminar/Form1.cs
+++ botseminar/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using VkNet;
+using VkNet.Model;
+using VkNet.Model.RequestParams;
 
 namespace botseminar
 {
@@ -17,17 +20,40 @@ namespace botseminar
         {
             InitializeComponent();
         }
-
+        public string s;
         private void button1_Click(object sender, EventArgs e)
         {
+            var api_user = new VkApi();
+            // обработать исключения!
+            api_user.Authorize(new ApiAuthParams
+            {
+                AccessToken = getAuthForUser()
+            });
+            var getFriends = api_user.Friends.Get(new VkNet.Model.RequestParams.FriendsGetParams
+            {
+                Fields = VkNet.Enums.Filters.ProfileFields.All
+            });
+            foreach (User user in getFriends)
+            {
+                s += user.FirstName + user.LastName + "\n";
+                s += user.Sex + "\n";
+                s += user.Relation + "\n";
+                s += user.HasMobile;
+                s += user.BirthDate;
+                listBox1.Items.Add(s);
+                s = "";
+            }
 
         }
+    
+
+
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
-        public static string getAuthForGroup()
+        public static string getAuthForUser()
         {
             string fileName = @"auth_vk.txt";
             string token = "";
